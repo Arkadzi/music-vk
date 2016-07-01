54f55f8df85f66fa68b9f6a71754cd46cf63b4aa
Arkadii <arkadii.humennyi@buki.com.ua>
2016-07-01 20:05:59 +0300

    split rest component

--- app/src/main/java/me/gumenniy/arkadiy/vkmusic/presentation/navigators/ServiceNavigator.java
+++ app/src/main/java/me/gumenniy/arkadiy/vkmusic/presentation/navigators/ServiceNavigator.java
@@ -9,9 +9,9 @@ import me.gumenniy.arkadiy.vkmusic.data.MusicService;
  * Created by sebastian on 01.07.16.
  */
 public class ServiceNavigator {
-    public void startService(A) {
-        Intent intent = new Intent(this, MusicService.class);
-        startService(Settings.Notification.ACTION.END_FOREGROUND_ACTION);
-        bindService(intent, connection, BIND_AUTO_CREATE);
-    }
+//    public void startService(A) {
+//        Intent intent = new Intent(this, MusicService.class);
+//        startService(Settings.Notification.ACTION.END_FOREGROUND_ACTION);
+//        bindService(intent, connection, BIND_AUTO_CREATE);
+//    }
 }
